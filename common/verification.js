4fdad9e966d82e9fa322079a529d6539dfa33108
Aelousdp <806267856@qq.com>
2018-02-06 05:38:00 +0800

    safari中Cookies不支持中文。
    Signed-off-by: Aelousdp <806267856@qq.com>

--- common/verification.js
+++ common/verification.js
@@ -1,15 +1,15 @@
 /*safari中cookies不支持中文，正在找寻编解码方式。*/
 var cPassWord = "160324";
 
-function setKey(c_name, value) {
-    document.cookie = c_name + "=" + value;
+function setKey(c_key, value) {
+    document.cookie = c_key + "=" + value;
 }
 
-function getKey(c_name) {
+function getKey(c_key) {
     if (document.cookie.length > 0) {
-        var c_start = document.cookie.indexOf(c_name + "=");
+        var c_start = document.cookie.indexOf(c_key + "=");
         if (c_start !== -1) {
-            c_start = c_start + c_name.length + 1;
+            c_start = c_start + c_key.length + 1;
             var c_end = document.cookie.indexOf(";", c_start);
             if (c_end === -1) c_end = document.cookie.length;
             return document.cookie.substring(c_start, c_end);
@@ -32,5 +32,5 @@ function checkKey() {
         }
     }
 }
-
+E
 checkKey();
