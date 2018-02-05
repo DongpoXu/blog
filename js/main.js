ca8f375a3a5a4156dee8d3a28b73b5eaf34444b6
Aelousdp <806267856@qq.com>
2018-02-06 07:36:30 +0800

    canvas时钟添加完毕
    Signed-off-by: Aelousdp <806267856@qq.com>

--- js/main.js
+++ js/main.js
@@ -50,18 +50,111 @@ var cbg = oClock.getContext('2d');
 var width = cbg.canvas.width;
 var height = cbg.canvas.height;
 var r = width / 2;
+var rem = width / 200;
 
 function drawBackground() {
+    cbg.save();
     cbg.translate(r, r);
     cbg.beginPath();
-    cbg.lineWidth = 10;
-    cbg.arc(0, 0, r - 5, 0, 2 * Math.PI, false);
+    cbg.lineWidth = 10 * rem;
+    cbg.arc(0, 0, r - cbg.lineWidth / 2, 0, 2 * Math.PI, false);
     cbg.stroke();
+    cbg.font = 18 * rem + "Arial";
+    cbg.textAlign = "center";
+    cbg.textBaseline = "middle";
+    var hourNumbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2];
+    hourNumbers.forEach(function (num, i) {
+        var rad = 2 * Math.PI / 12 * i;
+        var x = Math.cos(rad) * (r - 30 * rem);
+        var y = Math.sin(rad) * (r - 30 * rem);
+        cbg.fillText(num, x, y);
+    });
+
+    //标识点
+    for (var i = 0; i < 60; i++) {
+        var rad = 2 * Math.PI / 60 * i;
+        var x = Math.cos(rad) * (r - 18 * rem);
+        var y = Math.sin(rad) * (r - 18 * rem);
+        cbg.beginPath();
+        if (i % 5 === 0) {
+            cbg.fillStyle = "#000";
+            cbg.arc(x, y, 2 * rem, 0, 2 * Math.PI, false);
+        } else {
+            cbg.fillStyle = "#2b2bac";
+            cbg.arc(x, y, 2 * rem, 0, 2 * Math.PI, false);
+        }
+        cbg.fill();
+    }
 }
 
-drawBackground();
+//画时针
+function drawHour(hour, minute) {
+    cbg.save();
+    cbg.beginPath();
+    var rad = 2 * Math.PI / 12 * (hour + minute / 60);
+    cbg.rotate(rad);
+    cbg.lineWidth = 6 * rem;
+    cbg.lineCap = "round";
+    cbg.moveTo(0, 10 * rem);
+    cbg.lineTo(0, -r / 2);
+    cbg.stroke();
+    cbg.restore();
+}
 
+//画分针
+function drawMinute(minute) {
+    cbg.save();
+    cbg.beginPath();
+    var rad = 2 * Math.PI / 60 * minute;
+    cbg.rotate(rad);
+    cbg.lineWidth = 3 * rem;
+    cbg.lineCap = "round";
+    cbg.moveTo(0, 10 * rem);
+    cbg.lineTo(0, -r + 30 * rem);
+    cbg.stroke();
+    cbg.restore();
+}
 
+//画秒针
+function drawSecond(second) {
+    cbg.save();
+    cbg.beginPath();
+    cbg.fillStyle = "red";
+    var rad = 2 * Math.PI / 60 * second;
+    cbg.rotate(rad);
+    cbg.moveTo(-2 * rem, 20 * rem);
+    cbg.lineTo(2 * rem, 20 * rem);
+    cbg.lineTo(1, -r + 18 * rem);
+    cbg.lineTo(-1, -r + 18 * rem);
+    cbg.fill();
+    cbg.restore();
+}
+
+//中心点
+function drawDot() {
+    cbg.beginPath();
+    cbg.fillStyle = "white";
+    cbg.arc(0, 0, 3 * rem, 0, 2 * Math.PI, false);
+    cbg.fill();
+}
+
+//绘制
+function draw() {
+    cbg.clearRect(0, 0, width, height);
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    drawBackground();
+    drawHour(hour, minute);
+    drawMinute(minute);
+    drawSecond(second);
+    drawDot();
+    cbg.restore();
+}
+
+draw();
+setInterval(draw, 1000);
 
 
 
