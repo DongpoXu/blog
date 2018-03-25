793d3721aedaf63a224a712555446665c3b53330
Aelousdp <806267856@qq.com>
2018-03-25 22:03:34 +0800

    Chapter04--4.3(视频展示效果实例)---额外完成无缝对接，还差回翻。
    Signed-off-by: Aelousdp <806267856@qq.com>

--- learn/jQuery/锋利的jQuery/chapter04(jQuery中的事件和动画)/4.3(视频展示效果实例)/js/script.js
+++ learn/jQuery/锋利的jQuery/chapter04(jQuery中的事件和动画)/4.3(视频展示效果实例)/js/script.js
@@ -1,3 +1,68 @@
+/*
+ * 网站视频展示页效果实例
+ * 其实就是简单的轮播图界面
+ */
 $(function () {
+    var page = 1;
+    var i = 4;
+    $("span.next").click(function () {
+        var $show = $("div.videoContentList");       //要显示的内容
+        var $content = $("#videoContent");           //显示区域
 
+        var width = $content.width();               //显示区域宽度，即后期步进量
+        var len = $show.find("li").length;
+        var pageCount = Math.ceil(len / i);         //向下取整
+
+        if (!$show.is(":animated")) {
+            if (page === pageCount) {
+                // $show.animate({left: "0px"}, 0);   //跳转至第一个版面
+                // page = 1;
+            } else {
+                $show.animate({left: "-=" + width}, "slow");
+                page++;
+                if (page === pageCount) {
+                    /*
+                     * 这样可以实现无缝对接
+                     * 通过将最后一张图片设定为和第一张图片一样的模式
+                     * 到达最后一张图片之后
+                     * 缓慢做完动画效果
+                     * 然后快速切换至第一张即可
+                     */
+                    $show.animate({left: "0px"}, 0);   //跳转至第一个版面
+                    page = 1;
+                }
+            }
+            /*
+             * 此处需要解释
+             * 由于jQuery的动画并行
+             * 所以并不需要最后一张动画完毕才切换dot
+             * 可以方便的实现无缝对接
+             */
+            $("div.dot>ul>li").eq(page - 1).addClass("current").siblings().removeClass("current");
+        }
+    });
+    $("span.prev").click(function () {
+        var $show = $("div.videoContentList");       //要显示的内容
+        var $content = $("#videoContent");           //显示区域
+
+        var width = $content.width();               //显示区域宽度，即后期步进量
+        var len = $show.find("li").length;
+        var pageCount = Math.ceil(len / i);         //向下取整
+
+        if (!$show.is(":animated")) {
+            if (page === 1) {
+                // $show.animate({left: "0px"}, 0);   //跳转至第一个版面
+                // page = 1;
+            } else {
+                $show.animate({left: "+=" + width}, "slow");
+                page--;
+                if (page === pageCount) {
+                    //无缝对接方式同上
+                    $show.animate({left: "-=" + width * (pageCount - 1)}, 0);   //跳转至第一个版面
+                    page = pageCount;
+                }
+            }
+            $("div.dot>ul>li").eq(page - 1).addClass("current").siblings().removeClass("current");
+        }
+    });
 });
