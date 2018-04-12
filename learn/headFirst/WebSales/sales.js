c2282b5f326e893bbd5bb7c1b2a0efd196d335ab
Aelousdp <806267856@qq.com>
2018-04-12 18:33:01 +0800

    headfirst部分修改
    Signed-off-by: Aelousdp <806267856@qq.com>

--- learn/headFirst/WebSales/sales.js
+++ learn/headFirst/WebSales/sales.js
@@ -3,60 +3,68 @@ var lastReportTime = 0;
 window.onload = init;
 
 function init() {
-    // JSON
-    // var url = "https://xudongpo.xin/learn/WebSales/sales.json";
-    // var request = new XMLHttpRequest();
-    // request.open("GET",url);
-    // request.onload = function () {
-    //     if(request.status === 200){
-    //         updateSales(request.responseText);
-    //     }
-    // };
-    // request.send(null);
-    // 定时3秒获取一次
-    var interval = setInterval(handleRefresh,3000);
+    var interval = setInterval(handleRefresh, 3000);
     handleRefresh();
 }
 
+// window.onload = function () {
+// var url = "https://xudongpo.xin/learn/headFirst/WebSales/sales.json";
+// var request = new XMLHttpRequest();
+// //XMLHttpRequest Level 1
+// // request.open("GET", url);
+// // request.onload = function () {
+// //     if (request.status === 200) { //200表示OK
+// //         updateSales(request.responseText); //数据加载完成后，调用这个函数
+// //     }
+// // };
+// //XMLHttpRequest Level 2
+// request.onreadystatechange = function () {
+//     if (request.readyState === 4 && request.status === 200) { //200表示OK
+//         updateSales(request.responseText); //数据加载完成后，调用这个函数
+//     }
+// };
+// request.open("GET", url);
+// request.send(null); //最后发出请求
+/*以上为Ajax代码 */
+//     setInterval(handleRefresh, 2000);
+// }
+
 function handleRefresh() {
-    console.log("here");
     var url = "http://gumball.wickedlysmart.com" +
         "?callback=updateSales" +
         "&lastreporttime=" + lastReportTime +
         "&random=" + (new Date()).getTime();
 
     var newScriptElement = document.createElement("script");
-    newScriptElement.setAttribute("src",url);
-    newScriptElement.setAttribute("id","jsonp");
+    newScriptElement.setAttribute("src", url);
+    newScriptElement.setAttribute("id", "jsonp");
 
     var oldScriptElement = document.getElementById("jsonp");
     var head = document.getElementsByTagName("head")[0];
-    if(oldScriptElement == null){
+    if (oldScriptElement === null) {
         head.appendChild(newScriptElement);
-    }else {
-        head.replaceChild(newScriptElement,oldScriptElement);
+    } else {
+        head.replaceChild(newScriptElement, oldScriptElement);
     }
 }
 
 // function updateSales(responseText) {
 function updateSales(sales) {
     var salesDiv = document.getElementById("sales");
+    // salesDiv.innerHTML = responseText;
     // var sales = JSON.parse(responseText);
-    for(var i = 0; i < sales.length; i++){
-        var sale = sales[i];
+    for (var i = 0; i < sales.length; i++) {
+        var sale = sales[i];;
         var div = document.createElement("div");
-        div.setAttribute("class","saleItem");
-        div.innerHTML = sale.name + " sold " + sale.sales + " gumballs ";
-        // salesDiv.appendChild(div);
-        if (salesDiv.childElementCount === 0) {
+        div.setAttribute("class", "saleItem");
+        div.innerHTML = sale.name + "sold" + sale.sales + "gumballs";
+        if (salesDiv.childElementCount == 0) {
             salesDiv.appendChild(div);
-        }
-        else {
+        } else {
             salesDiv.insertBefore(div, salesDiv.firstChild);
         }
     }
-    if (sales.length > 0){
-        // mmp注意下面数组的sales不是sale......
-        lastReportTime = sales[sales.length-1].time;
+    if (sales.length > 0) {
+        lastReportTime = sales[sales.length - 1].time;
     }
 }
--- learn/jQuery/锋利的jQuery/chapter08(用jQuery打造个性网站)/js/main.js
+++ learn/jQuery/锋利的jQuery/chapter08(用jQuery打造个性网站)/js/main.js
@@ -29,12 +29,152 @@ $(function () {
     if (cookie_skin) {
         switchSkin(cookie_skin);
     }
+
+    function switchSkin(skinName) {
+        $("#" + skinName).addClass("selected")
+            .siblings().removeClass("selected");
+        $("#cssFile").attr("href", "css/skin/" + skinName + ".css");
+        $.cookie("MyCssSkin", skinName, {path: '/', expires: 10});
+    }
+
 });
 
-function switchSkin(skinName) {
-    $("#" + skinName).addClass("selected")
-        .siblings().removeClass("selected");
-    $("#cssFile").attr("href", "css/skin/" + skinName + ".css");
-    $.cookie("MyCssSkin", skinName, {path: '/', expires: 10});
+// 导航效果
+$(function () {
+    $("#nav li").hover(function () {
+        $(this).find(".jnNav").show();
+    }, function () {
+        $(this).find(".jnNav").hide();
+    });
+});
+
+// 左侧商品分类热销效果
+$(function () {
+    $(".jnCatainfo .promoted").append("<span class='hot'></span>");
+});
+
+// 中间大屏广告效果
+$(function () {
+    var $imgrolls = $("#jnImageroll div a");
+    $imgrolls.css("opacity", "0.7");
+    var len = $imgrolls.length;
+    var index = 0;
+    var adTimer = null;
+
+    $imgrolls.mouseover(function () {
+        index = $imgrolls.index(this);
+        showImg(index);
+    }).eq(0).mouseover();
+
+    // 滑入 停止动画，滑出 开始动画
+    $("#jnImageroll").hover(function () {
+        if (adTimer) {
+            clearInterval(adTimer);
+        }
+    }, function () {
+        adTimer = setInterval(function () {
+            index++;
+            // showImg(index);
+            if (index === len) {
+                index = 0;
+            }
+            showImg(index);     // showImg应该写在计算index完毕之后。
+        }, 5000);
+    }).trigger("mouseleave");
+
+    // 显示图片
+    function showImg(index) {
+        var $rollobj = $("#jnImageroll");
+        var $rolllist = $rollobj.find("div a");
+        var newhref = $rolllist.eq(index).attr("href");
+        $("#JS_imgWrap").attr("href", newhref)
+            .find("img").eq(index).stop(true, true).fadeIn()
+            .siblings().fadeOut();
+        $rolllist.removeClass("chos").css("opacity", "0.7")
+            .eq(index).addClass("chos").css("opacity", "1");
+    }
+});
+
+// 右侧最新动态模块内容添加超链接提示
+$(function () {
+    var x = 10;
+    var y = 20;
+    $("a.tooltip").mouseover(function (e) {
+        this.myTitle = this.title;
+        this.title = "";
+        var tooltip = "<div id='tooltip'>" + this.myTitle + "</div>";
+        $("body").append(tooltip);
+        $("#tooltip")
+            .css({
+                "top": (e.pageY + y) + "px",
+                "left": (e.pageX + x) + "px"
+            }).show("fast");
+    }).mouseout(function () {
+        this.title = this.myTitle;
+        $("#tooltip").remove();
+    }).mouseover(function (e) {
+        $("#tooltip")
+            .css({
+                "top": (e.pageY + y) + "px",
+                "left": (e.pageX + x) + "px"
+            });
+    });
+});
+
+// 右侧下部品牌活动横向滚动效果
+$(function () {
+    $("#jnBrandTab li a").click(function () {
+        $(this).parent().addClass("chos")
+            .siblings().removeClass("chos");
+        var idx = $("#jnBrandTab li a").index(this);
+        showBrandList(idx);
+        return false;
+    }).eq(0).click();
+});
+
+function showBrandList(index) {
+    var $rollobj = $("#jnBrandList");
+    var rollWidth = $rollobj.find("li").outerWidth();
+    rollWidth = rollWidth * 4;
+    $rollobj.stop(true, false).animate({
+        left: -rollWidth * index
+    }, 1000);
 }
 
+// 右侧下部光标划过产品列表效果
+/*
+ * live()方法在高版本被替代
+ * 使用on()方法
+ * 咋用额，，，是个问题。
+ * 所以用了笨办法
+ * 先清空所有的遮罩层，然后再添加
+ */
+$(function () {
+    $("#jnBrandList li").each(function (index) {
+        var $img = $(this).find("img");
+        var img_w = $img.width();
+        var img_h = $img.height();
+        var spanHtml = '<span title="cover" style="position:absolute;top:0;left:5px;width:' + img_w + 'px;height:' + img_h + 'px;" class="imageMask"></span>';
+        $(spanHtml).appendTo(this);
+    });
+    $("#jnBrandList li span[title=cover]").hover(function () {
+        $(this).addClass("imageOver");
+    },function () {
+        $(this).removeClass("imageOver");
+    });
+});
+
+/*使用jqzoom*/
+$(function(){
+    $('.jqzoom').jqzoom({
+        zoomType: 'standard',
+        lens:true,
+        preloadImages: false,
+        alwaysOn:false,
+        zoomWidth: 340,
+        zoomHeight: 340,
+        xOffset:10,
+        yOffset:0,
+        position:'right'
+    });
+});
