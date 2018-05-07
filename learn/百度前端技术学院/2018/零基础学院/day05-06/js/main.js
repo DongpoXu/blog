47ab2cf60db6b5654db4d78538fa1ef9919a7916
Aelousdp <806267856@qq.com>
2018-05-07 19:16:37 +0800

    转移位置
    Signed-off-by: Aelousdp <806267856@qq.com>

--- learn/百度前端技术学院/2018/零基础学院/day05-06/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    let oLink = document.getElementById("link");
-    let oBtn = document.getElementById("bar").getElementsByTagName("button");
-    for (let i = 0; i < oBtn.length; i++) {
-        oBtn[i].onclick = function () {
-            oLink.href = "css/" + this.innerHTML + ".css"
-        }
-    }
-};
